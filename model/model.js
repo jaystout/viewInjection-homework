f17ef51cd5b27cb1e62cbd938c9d81f1dcf3bfb2
jaystout <71277279+jaystout@users.noreply.github.com>
2020-10-12 00:46:48 -0400

    got rid of ../

--- model/model.js
+++ model/model.js
@@ -1,7 +1,7 @@
 var MODEL = (function (){
 //represents getting information from the database
 var _getView = function(viewName){
-    $.get(`/views/${viewName}/${viewName}.html`, function (data){ //template literal is backtick, `, to the left of 1 key.
+    $.get(`views/${viewName}/${viewName}.html`, function (data){ //template literal is backtick, `, to the left of 1 key.
         $("#app").html(data);
     });
 };
